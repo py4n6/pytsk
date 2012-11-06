95cbaedd071f6667b7713adce123611aa7410f92
Joachim Metz <joachim.metz@gmail.com>
2012-11-06 12:08:23 +0100

    Added dpkg files and update make_dist.sh accordingly

--- make_dist.sh
+++ make_dist.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 DATE=`date +"%Y-%m-%d"`;
+
 rm -f pytsk-*.tgz
-tar zcf pytsk-${DATE}.tgz ../pytsk/LICENSE ../pytsk/MANIFEST ../pytsk/*.* ../pytsk/msvscpp ../pytsk/samples 2>/dev/null
+
+FILES="../pytsk/LICENSE ../pytsk/MANIFEST ../pytsk/*.* ../pytsk/dpkg ../pytsk/msvscpp ../pytsk/samples"
+
+tar zcf pytsk-${DATE}.tgz ${FILES} 2>/dev/null
