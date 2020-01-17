e04ee6b83018168b1caf075287e19a3e0af4c036
Joachim Metz <joachim.metz@gmail.com>
2020-01-17 17:20:31 +0100

    Removed Python 2 CI tests

--- travis/install.sh
+++ travis/install.sh
@@ -10,16 +10,11 @@ then
 
 	docker run --name=${CONTAINER_NAME} --detach -i centos:${CENTOS_VERSION};
 
+	# Install packages.
 	docker exec ${CONTAINER_NAME} yum group install -y "Development Tools";
 
-	RPM_PACKAGES="git";
+	RPM_PACKAGES="git python3 python3-devel python3-setuptools";
 
-	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
-	then
-		RPM_PACKAGES="${RPM_PACKAGES} python2 python2-devel python2-setuptools";
-	else
-		RPM_PACKAGES="${RPM_PACKAGES} python3 python3-devel python3-setuptools";
-	fi
 	docker exec ${CONTAINER_NAME} yum install -y ${RPM_PACKAGES};
 
 	docker cp ../pytsk ${CONTAINER_NAME}:/
@@ -32,16 +27,11 @@ then
 
 	docker run --name=${CONTAINER_NAME} --detach -i registry.fedoraproject.org/fedora:${FEDORA_VERSION};
 
+	# Install packages.
 	docker exec ${CONTAINER_NAME} dnf groupinstall -y "Development Tools";
 
-	RPM_PACKAGES="autoconf automake gcc-c++ git libtool libstdc++-devel";
+	RPM_PACKAGES="autoconf automake gcc-c++ git libtool libstdc++-devel python3 python3-devel";
 
-	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
-	then
-		RPM_PACKAGES="${RPM_PACKAGES} python2 python2-devel";
-	else
-		RPM_PACKAGES="${RPM_PACKAGES} python3 python3-devel";
-	fi
 	docker exec ${CONTAINER_NAME} dnf install -y ${RPM_PACKAGES};
 
 	docker cp ../pytsk ${CONTAINER_NAME}:/
@@ -62,23 +52,19 @@ then
 	docker exec ${CONTAINER_NAME} locale-gen en_US.UTF-8;
 
 	# Install packages.
-	DPKG_PACKAGES="automake autotools-dev build-essential git libtool";
+	DPKG_PACKAGES="automake autotools-dev build-essential git libtool python3 python3-dev python3-distutils python3-setuptools";
 
-	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
-	then
-		DPKG_PACKAGES="${DPKG_PACKAGES} python python-dev python-setuptools";
-	else
-		DPKG_PACKAGES="${DPKG_PACKAGES} python3 python3-dev python3-distutils python3-setuptools";
-	fi
 	docker exec -e "DEBIAN_FRONTEND=noninteractive" ${CONTAINER_NAME} sh -c "apt-get install -y ${DPKG_PACKAGES}";
 
 	docker cp ../pytsk ${CONTAINER_NAME}:/
 
-elif test ${TARGET} = "linux";
+elif test ${TRAVIS_OS_NAME} = "linux";
 then
 	sudo apt-get update -q && sudo apt-get install -y autopoint;
 
-elif test ${TARGET} = "macos";
+elif test ${TRAVIS_OS_NAME} = "osx";
 then
 	brew update && brew install gettext && brew link --force gettext;
+
+        brew install python3 || true;
 fi
