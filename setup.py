d37f8b0501d230447fd6c0537fbf88a59485fe50
Joachim Metz <joachim.metz@gmail.com>
2020-01-17 19:29:37 +0100

    Changes for Mac OS build

--- setup.py
+++ setup.py
@@ -215,7 +215,7 @@ class BuildExtCommand(build_ext):
     command = [
         "sh", "configure", "--disable-java", "--without-afflib",
         "--without-libewf", "--without-libpq", "--without-libvhdi",
-        "--without-libvmdk", "--without-zlib", "CFLAGS=-std=c++11"]
+        "--without-libvmdk", "--without-zlib"]
 
     output = subprocess.check_output(command, cwd="sleuthkit")
     print_line = False
@@ -411,6 +411,7 @@ class ProjectBuilder(object):
     # The args for the extension builder.
     self.extension_args = {
         "define_macros": [],
+        "extra_compile_args": ["-std=c++11"],
         "include_dirs": ["talloc", self._libtsk_path, "sleuthkit", "."],
         "library_dirs": [],
         "libraries": []}
