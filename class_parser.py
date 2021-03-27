2e677566ed6aaf204301c2a2d1c1b154edfb3c2d
Joachim Metz <joachim.metz@gmail.com>
2021-03-27 13:21:49 +0100

    Updated to Sleuthkit 4.10.2 (#73)

--- class_parser.py
+++ class_parser.py
@@ -235,7 +235,7 @@ import lexer
 DEBUG = 0
 
 # The pytsk3 version.
-VERSION = "20210130"
+VERSION = "20210327"
 
 # These functions are used to manage library memory.
 FREE = "aff4_free"
--- setup.py
+++ setup.py
@@ -386,7 +386,7 @@ class UpdateCommand(Command):
 
   This is normally only run by packagers to make a new release.
   """
-  _SLEUTHKIT_GIT_TAG = "4.10.1"
+  _SLEUTHKIT_GIT_TAG = "4.10.2"
 
   version = time.strftime("%Y%m%d")
 
