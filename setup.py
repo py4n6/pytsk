a3fb72adf526be4ab3cbd20fef2e050e4cea7aaf
Joachim Metz <joachim.metz@gmail.com>
2023-10-07 03:51:01 -0700

    Updated to SleuthKit 4.12.1 (#97)

--- setup.py
+++ setup.py
@@ -241,7 +241,7 @@ class UpdateCommand(Command):
 
   This is normally only run by packagers to make a new release.
   """
-  _SLEUTHKIT_GIT_TAG = "4.12.0"
+  _SLEUTHKIT_GIT_TAG = "4.12.1"
 
   version = time.strftime("%Y%m%d")
 
