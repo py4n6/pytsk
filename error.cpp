19ac0fd7e67215c88e231b3cf522cdaeedf143de
greateggsgreg <36009512+greateggsgreg@users.noreply.github.com>
2026-04-29 14:05:45 -0400

    Changes for thread safety on Windows (#118)

--- error.cpp
+++ error.cpp
@@ -14,63 +14,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
-
-#if !defined( WIN32 )
-#include <pthread.h>
-#endif
 
 #include "aff4_errors.h"
 #include "class.h"
 
 #define ERROR_BUFF_SIZE 10240
 
-// Windows version not truely threadsafe for now
-#if defined( WIN32 )
-static char global_error_buffer[ERROR_BUFF_SIZE];
-static int global_error_type = 0;
-#else
-/** These slots carry the TLS error keys */
-static pthread_key_t error_str_slot;
-static pthread_once_t error_once = PTHREAD_ONCE_INIT;
+/* Per-thread error state. Use C++11 thread_local storage class to 
+ * guarantee each thread gets its own zero-initialized copy, so
+ * raising or inspecting errors never races across threads on any
+ * supported platform.
+ */
+static thread_local char tls_error_buffer[ERROR_BUFF_SIZE];
+static thread_local int tls_error_type = 0;
 
-static pthread_key_t error_value_slot;
-#endif
-
-#if defined( WIN32 )
-static void error_init(void) {
-  memset(global_error_buffer, 0, sizeof(global_error_buffer));
-};
-
-#else
-static void error_init(void);
-
-void error_dest(void *slot) {
-  if(slot) talloc_free(slot);
-};
-
-void error_init(void) {
-  // We create the error buffer slots
-  if(pthread_key_create(&error_str_slot, error_dest) ||
-     pthread_key_create(&error_value_slot, error_dest)) {
-    printf("Unable to set up TLS variables\n");
-    abort();
+DLL_PUBLIC int *aff4_get_current_error(char **error_buffer) {
+  if(error_buffer != NULL) {
+    *error_buffer = tls_error_buffer;
   };
+  return &tls_error_type;
 };
-#endif
 
 DLL_PUBLIC void *aff4_raise_errors(int t, const char *reason, ...) {
   char *error_buffer;
   char tmp[ERROR_BUFF_SIZE];
-  // This has to succeed:
   int *type = aff4_get_current_error(&error_buffer);
+
+  /* Always leave tmp as a valid C string: vsnprintf is skipped when
+   * reason is NULL, and strncat below reads from tmp unconditionally.
+   */
+  tmp[0] = 0;
 
   if(reason) {
     va_list ap;
     va_start(ap, reason);
 
-    vsnprintf(tmp, ERROR_BUFF_SIZE-1, reason,ap);
-    tmp[ERROR_BUFF_SIZE-1]=0;
+    vsnprintf(tmp, ERROR_BUFF_SIZE-1, reason, ap);
+    tmp[ERROR_BUFF_SIZE-1] = 0;
     va_end(ap);
   };
 
@@ -87,39 +70,3 @@ DLL_PUBLIC void *aff4_raise_errors(int t, const char *reason, ...) {
 
   return NULL;
 };
-
-#if defined( WIN32 )
-DLL_PUBLIC int *aff4_get_current_error(char **error_buffer) {
-  if(error_buffer != NULL) {
-    *error_buffer = global_error_buffer;
-  };
-  return &global_error_type;
-};
-
-#else
-DLL_PUBLIC int *aff4_get_current_error(char **error_buffer) {
-  int *type;
-
-  (void) pthread_once(&error_once, error_init);
-  type = (int *) pthread_getspecific(error_value_slot);
-
-  // This is optional
-  if(error_buffer != NULL) {
-    *error_buffer = (char *) pthread_getspecific(error_str_slot);
-
-    // If TLS buffers are not set we need to create them
-    // TODO: the TLS buffers need to be freed on exit.
-    if(*error_buffer == NULL) {
-      *error_buffer = (char *) talloc_size(NULL, ERROR_BUFF_SIZE);
-      pthread_setspecific(error_str_slot, *error_buffer);
-    };
-  };
-
-  if(!type) {
-    type = (int *) talloc_size(NULL, ERROR_BUFF_SIZE);
-    pthread_setspecific(error_value_slot, type);
-  };
-
-  return type;
-};
-#endif
